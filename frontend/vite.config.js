835838411e407b34bffe2b813b996f3f5f3514a7
Lalit Rout <lalitsnrout@gmail.com>
2025-03-12 00:21:47 +0530

    Fix refresh issue on Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // Ensures React Router works in dev mode
-  },
-  build: {
-    outDir: "dist",
-  },
   resolve: {
     alias: {
       "@": "/src",
     },
   },
+  build: {
+    outDir: "dist",
+  }
 });
