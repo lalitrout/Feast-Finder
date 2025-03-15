735e69c2f528bbd4fa53c70d0f082f16c23fae95
Lalit Rout <lalitsnrout@gmail.com>
2025-03-15 10:26:08 +0530

    Fix refresh issue on Vercel

--- backend/models/EventInfo.js
+++ backend/models/EventInfo.js
@@ -5,12 +5,9 @@ const EventInfoSchema = new Schema({
     name: { type: String, required: true },
     location: { type: mongoose.Schema.Types.Mixed, required: true },
     date: { type: Date, required: true },
-    img: {
-        url: { type: String, required: true }, // Cloudinary Image URL
-        public_id: { type: String, required: true }, // Cloudinary Image ID for deletion
-    },
+    img: { type: String, required: true },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Link event to user
-}, { timestamps: true });
+});
 
 const EventInfo = mongoose.model("EventInfo", EventInfoSchema);
 module.exports = EventInfo;
